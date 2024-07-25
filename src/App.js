2817eb05c9e60674229042e34626ea598c8cf523
Kulam9 <kulam.521587589@gmail.com>
2024-07-25 17:34:31 +0300

    /

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+// Декларативный стиль
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+         {new Date().getFullYear()}  {/* Императивный стиль */}
+        </p>
       </header>
     </div>
   );
