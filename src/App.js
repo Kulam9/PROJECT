724ccfc48c597164581a849bd8f93074c853a1e5
Kulam9 <kulam.521587589@gmail.com>
2024-07-25 17:38:22 +0300

    NoJSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// Декларативный стиль
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-         {new Date().getFullYear()}  {/* Императивный стиль */}
-        </p>
-      </header>
-    </div>
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      'header',
+      { className: 'App-header' },
+      React.createElement('img', {
+        src: logo,
+        className: 'App-logo',
+        alt: 'logo',
+      }),
+      React.createElement(
+        'p',
+        null,
+        'Edit ',
+        React.createElement('code', null, 'src/App.js'),
+        ' and save to reload.'
+      ),
+      React.createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        'Learn React'
+      ),
+      React.createElement('p', null, new Date().getFullYear())
+    )
   );
 }
 
 export default App;
+
